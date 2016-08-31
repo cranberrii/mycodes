72865617b97a8b375f3bb5960c0977b391eb2d00
cranberrii <jermynbek@gmail.com>
2016-08-31 21:10:00 +0800

    Add files via upload

--- attrition_prediction.R
+++ attrition_prediction.R
@@ -15,6 +15,8 @@ library(gbm)
 library(caret)
 library(pROC)
 library(e1071)
+library(tibble)
+library(readr)
 
 dev.off()
 
@@ -30,7 +32,7 @@ df$MIN <- as.numeric(gsub("[[:punct:]]", "", df$MIN))
 df$MID <- as.numeric(gsub("[[:punct:]]", "", df$MID))
 df$MAX <- as.numeric(gsub("[[:punct:]]", "", df$MAX))
 df$RANGE.PENETRATION <- as.numeric(sub("%","",df$RANGE.PENETRATION))
-df$RANGE.PENETRATION[df$RANGE.PENETRATION >400] <- mean(df$RANGE.PENETRATION)
+df$RANGE.PENETRATION[df$RANGE.PENETRATION >400] <- 399
 df$RANGE.PENETRATION.2 <- cut(df$RANGE.PENETRATION, 
                        breaks = c(-Inf, 0, 33, 66, 100, Inf), 
                        labels = c("< 0%", "0% to 33%", "34% to 66%", "67% to 100%", "> 100%"), 
@@ -51,14 +53,17 @@ df$PROMO.1.YEAR.2 <- cut(df$PROMO.1.YEAR, breaks = c(-Inf, 1, Inf),
                                    labels = c(0, 1), right = FALSE)
 df <- df %>% 
   separate(col = COST.CENTER, into = c("CC1", "CC2","CC3"), sep = "/") %>%
-  filter(JOB.GRADE != "10" & Type.of.Contract == "localcontract" & MainLoc == "Vietnam") %>%
+  filter(JOB.GRADE != "10" & Type.of.Contract == "localcontract" ) %>%
   filter(Transfer == 0 & Leaverin3months == 0 & MID != "NA")
-df$CC1 <- as.factor(gsub(" ", "", df$CC1))
-df$CC2 <- as.factor(gsub(" ", "", df$CC2))
-df$CC3 <- as.factor(gsub(" ", "", df$CC3))
+df$CC1 <- as.factor(tolower(gsub(" ", "", df$CC1)))
+df$CC2 <- as.factor(tolower(gsub(" ", "", df$CC2)))
+df$CC3 <- as.factor(tolower(gsub(" ", "", df$CC3)))
+df <- df %>% 
+  filter(CC1 == 'technology')
+
 # SELECT only variables for prediction
 df <- df %>% 
-  select(CC1, Length.of.Service, JOB.GRADE, 
+  select(Length.of.Service, JOB.GRADE, 
          PROMO.1.YEAR.2, SalaryIncrementin1Year.2, EmployeeAge, 
          EducationLevel, MaritalStatus, No.ofDeps, 
          RANGE.PENETRATION.2, Age.LOS, RP.Age, 
@@ -67,7 +72,7 @@ df <- df %>%
   # low importance: MainLoc
 
 
-set.seed(255)
+set.seed(345)
 
 split = sample.split(df$Leaver.2, SplitRatio = 0.70)
 df.train = subset(df, split==TRUE)
@@ -87,16 +92,30 @@ prop.table(table(df$RANGE.PENETRATION.2, df$Leaver.2),1)
 df %>% 
   group_by(RANGE.PENETRATION.2, Leaver.2) %>%
   count(RANGE.PENETRATION.2) %>%
-  mutate(freq = n/sum(n))
+  mutate(freq = n/sum(n)) %>%
+  ggplot(aes(x = RANGE.PENETRATION.2, fill = Leaver.2)) + geom_bar()
 
-ggplot(data = df, aes(x = Length.of.Service, fill = Leaver.2)) + geom_density(alpha=0.33)
+df %>% 
+  group_by(RANGE.PENETRATION.2, Leaver.2) %>%
+  count(RANGE.PENETRATION.2) %>%
+  ggplot(aes(x = RANGE.PENETRATION.2, fill = Leaver.2)) + geom_bar()
+
+
+ggplot(data = df, aes(x = Length.of.Service, fill = Leaver.2)) + geom_density(alpha=0.33) + 
+  theme(axis.title.y=element_text(size=16), axis.title.x=element_text(size=16), axis.text.y  = element_text(size=16), axis.text.x  = element_text(size=16))
 ggplot(data = df, aes(x = Length.of.Service, fill = Leaver.2)) + geom_histogram(alpha=0.5, binwidth=1)
 
-ggplot(data = df, aes(x = EmployeeAge, fill = Leaver.2)) + geom_density(alpha=0.33)
+ggplot(data = df, aes(x = EmployeeAge, fill = Leaver.2)) + geom_density(alpha=0.33) + 
+  theme(axis.title.y=element_text(size=16), axis.title.x=element_text(size=16), axis.text.y  = element_text(size=16), axis.text.x  = element_text(size=16))
+
 
 ggplot(data = df, aes(x = RANGE.PENETRATION, fill = PROMO.1.YEAR.2)) + geom_density(alpha=0.33)
 
-ggplot(data = df, aes(x = RANGE.PENETRATION.2, ..count..)) + geom_bar(aes(fill = Leaver.2), position = 'fill')
+ggplot(data = df, aes(x = RANGE.PENETRATION.2)) + geom_bar(aes(fill = Leaver.2), position = 'fill') + 
+  #facet_wrap(~CC1) + 
+  theme(axis.title.y=element_text(size=13), axis.title.x=element_text(size=13), 
+        axis.text.y  = element_text(size=13), axis.text.x  = element_text(size=11),
+        strip.text.x = element_text(size=13),strip.text.y = element_text(size=13))
 
 
 ###cart###
